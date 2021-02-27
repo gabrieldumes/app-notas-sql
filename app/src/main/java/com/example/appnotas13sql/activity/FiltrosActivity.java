40c7737106adb765f5477ad3a88fd3742d1291ac
Gabriel Dümes <gabrieldumes@users.noreply.github.com>
2021-02-27 13:59:21 -0300

    Recurso salvar filtros

--- app/src/main/java/com/example/appnotas13sql/activity/FiltrosActivity.java
+++ app/src/main/java/com/example/appnotas13sql/activity/FiltrosActivity.java
@@ -6,15 +6,69 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.example.appnotas13sql.R;
+import com.example.appnotas13sql.helper.ArmazenamentoPreferencias;
 
 public class FiltrosActivity extends AppCompatActivity {
+
+    private RadioGroup radioGroupOrdenar;
+    private RadioButton radioMaisRecentes, radioMaisAntigas;
+    private EditText editPesquisa;
+    private CheckBox checkNotas, checkLembretes;
+    private Button buttonAplicarFiltros;
+
+    private ArmazenamentoPreferencias preferencias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filtros);
+
+        radioGroupOrdenar = findViewById(R.id.radioGroupOrdenar);
+        radioMaisRecentes = findViewById(R.id.radioMaisRecentes);
+        radioMaisAntigas = findViewById(R.id.radioMaisAntigas);
+        editPesquisa = findViewById(R.id.editPesquisa);
+        checkNotas = findViewById(R.id.checkNotas);
+        checkLembretes = findViewById(R.id.checkLembretes);
+        buttonAplicarFiltros = findViewById(R.id.buttonAplicarFiltros);
+
+        preferencias = new ArmazenamentoPreferencias(getApplicationContext());
+
+        recuperarFiltros();
+
+        buttonAplicarFiltros.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkNotas.isChecked() || checkLembretes.isChecked()) {
+                    preferencias.setFiltros(
+                            radioGroupOrdenar.getCheckedRadioButtonId(),
+                            editPesquisa.getText().toString(),
+                            checkNotas.isChecked(),
+                            checkLembretes.isChecked()
+                    );
+                    Toast.makeText(FiltrosActivity.this, "Filtros aplicados", Toast.LENGTH_SHORT).show();
+                    finish();
+                } else {
+                    Toast.makeText(FiltrosActivity.this, "Selecione onde aplicar os filtros", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    public void recuperarFiltros() {
+        radioGroupOrdenar.check(preferencias.getOrdenacao());
+        editPesquisa.setText(preferencias.getPesquisa());
+        checkNotas.setChecked(preferencias.isFiltroEmNotas());
+        checkLembretes.setChecked(preferencias.isFiltroEmLembretes());
     }
 
     @Override
---
+++ app/src/main/java/com/example/appnotas13sql/helper/ArmazenamentoPreferencias.java
@@ -0,0 +1,52 @@
+package com.example.appnotas13sql.helper;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.example.appnotas13sql.R;
+
+public class ArmazenamentoPreferencias {
+
+    private Context context;
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+
+    private final String ARQUIVO_PREFERENCIAS = "app_preferencias";
+
+    private final String CHAVE_ORDENACAO = "ordenacao";
+    private final String CHAVE_PESQUISA = "pesquisa";
+    private final String CHAVE_FILTRO_EM_NOTAS = "em_notas";
+    private final String CHAVE_FILTRO_EM_LEMBRETES = "em_lembretes";
+
+    public ArmazenamentoPreferencias(Context context) {
+        this.context = context;
+        preferences = context.getSharedPreferences(ARQUIVO_PREFERENCIAS, 0);
+        editor = preferences.edit();
+    }
+
+    //------> FILTROS <------
+
+    public void setFiltros(int ordenacao, String pesquisa, boolean emNotas, boolean emLembretes) {
+        editor.putInt(CHAVE_ORDENACAO, ordenacao);
+        editor.putString(CHAVE_PESQUISA, pesquisa);
+        editor.putBoolean(CHAVE_FILTRO_EM_NOTAS, emNotas);
+        editor.putBoolean(CHAVE_FILTRO_EM_LEMBRETES, emLembretes);
+        editor.commit();
+    }
+
+    public int getOrdenacao() {
+        return preferences.getInt(CHAVE_ORDENACAO, R.id.radioMaisRecentes);
+    }
+
+    public String getPesquisa() {
+        return preferences.getString(CHAVE_PESQUISA, "");
+    }
+
+    public boolean isFiltroEmNotas() {
+        return preferences.getBoolean(CHAVE_FILTRO_EM_NOTAS, false);
+    }
+
+    public boolean isFiltroEmLembretes() {
+        return preferences.getBoolean(CHAVE_FILTRO_EM_LEMBRETES, false);
+    }
+}
