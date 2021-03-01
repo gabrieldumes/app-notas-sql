7c2dfa6a52f95398a89a65629bf945990d8ca8b0
Gabriel Dümes <gabrieldumes@users.noreply.github.com>
2021-03-01 08:27:08 -0300

    Melhorias na tela de configurações

--- app/src/main/java/com/example/appnotas13sql/ui/ConfiguracoesFragment.java
+++ app/src/main/java/com/example/appnotas13sql/ui/ConfiguracoesFragment.java
@@ -7,14 +7,36 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.example.appnotas13sql.R;
 
 public class ConfiguracoesFragment extends Fragment {
 
+    private SeekBar seekTamanhoFonte;
+    private RadioGroup radioGroupCorFundoNotas;
+    private RadioButton radioCorAmarelo, radioCorVerde, radioCorCinza;
+    private Button buttonExportarNotas;
+    private TextView textConfigResetarFiltros, textDeletarDados, textRestaurarConfiguracoes;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_configuracoes, container, false);
+
+        seekTamanhoFonte = view.findViewById(R.id.seekTamanhoFonte);
+        radioGroupCorFundoNotas = view.findViewById(R.id.radioGroupCorFundoNotas);
+        radioCorAmarelo = view.findViewById(R.id.radioCorAmarelo);
+        radioCorVerde = view.findViewById(R.id.radioCorVerde);
+        radioCorCinza = view.findViewById(R.id.radioCorCinza);
+        buttonExportarNotas = view.findViewById(R.id.buttonExportarNotas);
+        textConfigResetarFiltros = view.findViewById(R.id.textConfigResetarFiltros);
+        textDeletarDados = view.findViewById(R.id.textDeletarDados);
+        textRestaurarConfiguracoes = view.findViewById(R.id.textRestaurarConfiguracoes);
+
         return view;
     }
 }
